2260d7ebea26156a634da6750796422b9fbae3b0
Gevendra Sahu <gevendra2004@gmail.com>
2023-07-28 20:15:37 +0530

    Contact section added

--- js/main.js
+++ js/main.js
@@ -55,6 +55,45 @@ let swiperTestimonial = new Swiper(".testimonial-container", {
   });
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      contactProject = document.getElementById('contact-project'),
+      contactMessage = document.getElementById('contact-message')
+
+// const sendEmail = (email) =>{
+//   email.preventDefault()
+
+//   // Check if the field has a value
+//   if(contactName.value === '' || contactEmail.value === '' || contactProject.value === ''){
+//     // Add and remove color
+//     contactMessage.classList.remove('color-blue')
+//     contactMessage.classList.add('color-red')
+
+//     // Show message	
+//     contactMessage.textContent = 'Write all the input fields 📩'
+//   }else{
+//     // serviceID - templateID - #form - publicKey
+//     emailjs.sendForm('','','','')
+//           .then(() =>{
+//             contactMessage.classList.add('color-blue')
+//             contactMessage.textContent = 'Message sent ✅'
+
+//             // Remove message after 5 sec
+//             setTimeout(()=> {
+//               contactMessage.textContent = ''
+//             }, 5000)
+//           } , (error) =>{
+//             alert('OOPS! SOMETHING HAS FAILED...' , error)
+//           })
+
+//           // to clear the input field 
+//           contactName.value = ''
+//           contactEmail.value = ''
+//           contactProject.value = ''
+//   }
+// }
+// contactForm.addEventListener('submit' , sendEmail)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
