541ddfc731009acf6360a51fd22e930a27ea3b79
Gevendra Sahu <gevendra2004@gmail.com>
2023-07-29 11:25:18 +0530

    Footer

--- js/main.js
+++ js/main.js
@@ -97,7 +97,25 @@ const contactForm = document.getElementById('contact-form'),
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+const scrollActive = () =>{
+  const scrollY = window.pageYOffset
+
+  sections.forEach( current => {
+    const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 58,
+          sectionId= current.getAttribute('id'),
+          sectionClass = document.querySelector('.nav-menu a[href*=' + sectionId + ']')
+
+          if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            sectionClass.classList.add('active-link')
+          }else{
+            sectionClass.classList.remove('active-link')
+          }
+  })
+}
+window.addEventListener('scroll' , scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
