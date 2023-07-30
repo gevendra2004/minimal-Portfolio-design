320192fc53eb3c111ec8c1d64d8902e5d6220831
Gevendra Sahu <gevendra2004@gmail.com>
2023-07-30 18:14:51 +0530

    update

--- js/main.js
+++ js/main.js
@@ -162,7 +162,7 @@ window.addEventListener('scroll' , scrollHeader)
 const sr = ScrollReveal({
   origin: 'top',
   distance: '60px',
-  duration: 2500,
+  duration: 2000,
   delay: 400,
   // reset: true /*Animation repeat*/
 })
